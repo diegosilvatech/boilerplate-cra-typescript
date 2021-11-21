4ecb3e65a6ccf249fa490b5499933c211a99bb72
coderamos <diego.coderamos@gmail.com>
2021-11-21 18:13:19 -0300

    feat: update button story

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import Button from '.';
 
 export default {
-  title: 'Button',
+  title: 'Atom/Button',
   component: Button,
   args: {
     variant: 'primary'
