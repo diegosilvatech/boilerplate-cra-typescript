a52ce205a5ac59cc9c3e7c7355b8ed8c8fa75f71
coderamos <diego.coderamos@gmail.com>
2022-04-11 19:32:06 -0300

    feat: update Form component

--- src/components/Form/FormLogin/stories.tsx
+++ src/components/Form/FormLogin/stories.tsx
@@ -3,8 +3,9 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import FormLogin, { FormLoginProps } from '.';
 
 export default {
-  title: 'FormLogin',
-  component: FormLogin
+  title: 'Organisms/FormLogin',
+  component: FormLogin,
+  onSubmit: () => null
 } as Meta;
 
 export const FormLoginDefault: Story<FormLoginProps> = (args) => (
